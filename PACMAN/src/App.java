10e772d9be23c8800e3a80f0cf69419ec243467f
AdityaGaikwad <adityagkwd.90@gmail.com>
2025-07-28 23:31:17 +0530

    Second Commit

--- PACMAN/src/App.java
+++ PACMAN/src/App.java
@@ -1,5 +1,19 @@
+import javax.swing.JFrame;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardheight = rowCount * tileSize;
+
+        JFrame frame = new JFrame("PacMan");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardheight);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
     }
 }
